b353eff66a13cbb6148789112c9063a7998cf7ff
Kemal Delalic <kemal.delalic@gmail.com>
2011-10-30 10:45:15 +0100

    3.2 fixes

--- classes/kohana/gravatar.php
+++ classes/kohana/gravatar.php
@@ -91,23 +91,23 @@ class Kohana_Gravatar {
 
 		if (empty($config))
 		{
-			$this->_config = Kohana::config('gravatar.default');
+			$this->_config = Kohana::$config->load('gravatar.default');
 		}
 		elseif (is_array($config))
 		{
 			// Setup the configuration
-			$config += Kohana::config('gravatar.default');
+			$config += Kohana::$config->load('gravatar.default');
 			$this->_config = $config;
 		}
 		elseif (is_string($config))
 		{
-			if ($config = Kohana::config('gravatar.'.$config) === NULL)
+			if ($config = Kohana::$config->load('gravatar.'.$config) === NULL)
 			{
 				throw new Gravatar_Exception('Gravatar.__construct() , Invalid configuration group name : :config', 
 					array(':config' => $config));
 			}
 
-			$this->_config = $config + Kohana::config('gravatar.default');
+			$this->_config = $config + Kohana::$config->load('gravatar.default');
 		}
 	}
 
@@ -187,7 +187,7 @@ class Kohana_Gravatar {
 		}
 		else
 		{
-			if (validate::url($url))
+			if (Valid::url($url))
 			{
 				$this->_config['default'] = $url;
 			}
